189e3e0b52bd0e11bc282b76257e4b5d454af218
shailesh00010111 <shailesh@nvestbank.com>
2022-09-28 20:00:29 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.scss";
+import Homepage from './Pages/Homepage';
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Homepage />
+  </div>;
 }
 
 export default App;
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import Footer from '../Components/Footer/Footer'
+import Header from '../Components/Header/Header'
+import Layout from '../Components/Layout/Layout'
 
 export default function Homepage() {
   return (
-    <div>Homepage</div>
+    <>
+      <Header /> 
+      <Footer />
+    </>
+  )
+}
+
+export default function Homepage() {
+  return (
+    <Layout>
+      <div>
+        
+      </div>
+    </Layout>
   )
 }
