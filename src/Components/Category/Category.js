772b89f6eb2b5617d9de89fd6b1a1dce7edd1c7e
shailesh00010111 <shailesh@nvestbank.com>
2022-10-14 00:07:20 +0530

    home

---
+++ src/Components/Category/Category.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./category.scss"
+export default function Category() {
+  return (
+    <div className="category-main">
+        <h5>Category title</h5>
+        <div className="category-products-wrapper">
+
+        </div>
+
+    </div>
+  )
+}
